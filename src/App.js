5b9efe2dd5427682ac5ab75a868700e41dbb25a4
RohaanPawar <rohanpawar07@gmail.com>
2020-04-26 12:12:24 +0530

    updated project sturcture

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <div>
+            <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+            <div class="flex-shrink-0">
+                <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+            </div>
+            <div class="ml-6 pt-1">
+                <h4 class="text-xl text-gray-900 leading-tight">ChitChat</h4>
+                <p class="text-base text-gray-600 leading-normal">You have a new message!</p>
+            </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
